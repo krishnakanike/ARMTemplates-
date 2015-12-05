f25cb2b0145af85035f67a2ef268e7205cb15688
krishnakanike <sumank1435@gmail.com>
2015-12-04 16:17:22 -0800

    Update DeployThruARM.ps1
    Added few more conditions check, passing dynamic parameters from WebHook.

--- DeployThruARM.ps1
+++ DeployThruARM.ps1
@@ -1,20 +1,68 @@
-#Connect to your Azure account
-Add-AzureRmAccount 
+param(
+	[object] $WebhookData	
+)
+# If runbook was called from Webhook, WebhookData will not be null.
+if ($WebhookData -ne $null) {   
 
-#Select your subscription if you have more than one
-#Select-AzureRmSubscription -SubscriptionId "Your subcription ID here"
-
-#Create a GUID for the job
-$JobGUID = [System.Guid]::NewGuid().toString()
-
-#Set the parameter values for the template - edit the parameters
-$Params = @{
-    "accountName" = "MyAccount2" ;
-    "regionId" = "Japan East";
-	"userName" = "Your UserName"; 
-	"password" = "Your password";
+    # Collect properties of WebhookData
+    $WebhookName    =   $WebhookData.WebhookName
+    $WebhookHeaders =   $WebhookData.RequestHeader
+    $WebhookBody    =   $WebhookData.RequestBody
+		
+	# Collect individual headers. VMList converted from JSON.
+	$From = $WebhookHeaders.From
+    $ParamList = ConvertFrom-Json -InputObject $WebhookBody
+    Write-Output "Runbook started from webhook $WebhookName by $From."
+	Write-Output "Runbook ParamList from webhook $WebhookName are "
+	Write-Output $ParamList
+			
+	#Set the parameter values required for creation of a Resource group
+	$ResourceGroupName = $ParamList.ResourceGroupName
+	$ResourceGrpLocation = $ParamList.ResourceGrpLocation
+	
+	#Set the parameter values for the template - edit the parameters
+	$Params = @{
+    "location" = $ParamList.location;
+	"dmzdemofevmName" = $ParamList.dmzdemofevmName;
+	"dmzdemofevmAdminUserName" = $ParamList.dmzdemofevmAdminUserName;
+	"dmzdemofevmAdminPassword" = $ParamList.dmzdemofevmAdminPassword;
+	"dmzdemoappvmName" = $ParamList.dmzdemoappvmName;
+	"dmzdemoappvmAdminUserName" = $ParamList.dmzdemoappvmAdminUserName;
+	"dmzdemoappvmAdminPassword" = $ParamList.dmzdemoappvmAdminPassword; 
+	"dmzdemodbvmName" = $ParamList.dmzdemodbvmName; 
+	"dmzdemodbvmAdminUserName" = $ParamList.dmzdemodbvmAdminUserName; 
+	"dmzdemodbvmAdminPassword" = $ParamList.dmzdemodbvmAdminPassword;
+	"dmzdemofevmpipDnsName" = $ParamList.dmzdemofevmpipDnsName;
+	"dmzdemoappvmpipDnsName" = $ParamList.dmzdemoappvmpipDnsName;
+	"dmzdemodbvmpipDnsName" = $ParamList.dmzdemodbvmpipDnsName;
+    }
 }
+ else {
+        Write-Error "Runbook mean to be started only from webhook." 
+    } 
 
-$TemplateURI = "https://raw.githubusercontent.com/krishnakanike/ARMTemplates-/master/DMZ_NSG_ARMTemplate.json"
-New-AzureRMResourceGroupDeployment -TemplateParameterObject $Params -ResourceGroupName "krishnadeploy1" -TemplateUri $TemplateURI
+#The name of the Automation Credential Asset this runbook will use to authenticate to Azure. 
+$CredentialAssetName = 'usvmtask@microsoft.com' 
+
+#Get the credential with the above name from the Automation Asset store 
+$Cred = Get-AutomationPSCredential -Name $CredentialAssetName 
+if(!$Cred) { 
+    Throw "Could not find an Automation Credential Asset named '${CredentialAssetName}'. Make sure you have created one in this Automation Account." 
+} 
+
+#Connect to your Azure Account 
+$Account = Add-AzureRmAccount -Credential $Cred 
+if(!$Account) { 
+    Throw "Could not authenticate to Azure using the credential asset '${CredentialAssetName}'. Make sure the user name and password are correct." 
+} 
+ 
+$TemplateURI = $ParamList.ARMTemplateURI
+
+# Create a new Resource Group
+New-AzureRmResourceGroup -Name $ResourceGroupName -Location $ResourceGrpLocation
+Write-Output "Runbook command New-AzureRmResourceGroup with Name $ResourceGroupName and location $ResourceGrpLocation completed"
+
+# Deploy the infrastructure from above paramlist and ARM template URI
+New-AzureRMResourceGroupDeployment -TemplateParameterObject $Params -ResourceGroupName $ResourceGroupName -TemplateUri $TemplateURI
+Write-Output "Runbook command New-AzureRMResourceGroupDeployment with above ParamList and ARMTemplateURI $TemplateURI completed"
 
